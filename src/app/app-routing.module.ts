0a3ed4101f735ef0dfac96106e2414c628b6dc50
Wei-jye Wang <weijye91@live.com>
2020-01-13 18:23:57 +0800

    feat(app): add route to contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
