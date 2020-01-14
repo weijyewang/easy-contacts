4d2a509909b9eff4bba516b34ffa530a59026502
Wei-Jye Wang <weijye91@live.com>
2020-01-15 00:15:57 +0800

    feat(add-contacts): add base code for add contacts component

---
+++ src/app/add-contacts/add-contacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-add-contacts',
+	templateUrl: './add-contacts.component.html',
+	styleUrls: ['./add-contacts.component.scss']
+})
+export class AddContactsComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
