f03f64acc07db1121b8f7c26eac57aba58f3fbb3
Wei-jye Wang <weijye91@live.com>
2020-01-15 19:03:49 +0800

    feat(contact-form, view-contacts): add layout for the mainpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { ViewContactsComponent } from './view-contacts/view-contacts.component';
 import { AddContactsComponent } from './add-contacts/add-contacts.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ViewContactsComponent,
 		AddContactsComponent,
-		HeaderComponent
+		HeaderComponent,
+		ContactFormComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		MatButtonModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
